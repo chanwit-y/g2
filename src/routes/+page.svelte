38ac3cca7c55c12eedc8a0ead748df3d3ebb91d9
cy <chanwit.yimneam@hotmail.com>
2023-10-08 10:57:17 +0700

    🦋

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
+	import { lazyLoad } from '$lib/lazyload';
+
+	let animate = '';
+	const handleSelectImage = () => {
+		animate = 'animate-ping';
+		setTimeout(() => {
+			animate = '';
+		}, 1000);
+	};
 </script>
 
 <div class="p-5 grid grid-cols-2 md:grid-cols-3 gap-4">
-	<div class="flex relative cursor-pointer" >
-		<div class="absolute right-0 w-2/3 h-full  opacity-80 rounded-r-lg  transition transform delay-150 ease-in-out hover:bg-slate-200 hover:duration-300 ">
-		</div>
+	<div class={`flex relative cursor-pointer ${animate}`} on:click={handleSelectImage}>
+		<div
+			class="absolute right-0 w-2/3 h-full opacity-80 rounded-r-lg transition transform delay-150 ease-in-out hover:bg-slate-200 hover:duration-300"
+		/>
 		<img
-			class="h-auto max-w-full rounded-lg"
-			src=""
+			class="h-auto max-w-full rounded-lg opacity-0 transition-opacity ease-in-out duration-300"
+			use:lazyLoad={''}
 			alt=""
 		/>
 	</div>
