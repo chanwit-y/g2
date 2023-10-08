5c52c05f2db5f12b21a32362c32589cb2bf19448
cy <chanwit.yimneam@hotmail.com>
2023-10-08 09:52:36 +0700

    🌓

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,43 @@
 <script lang="ts">
-	import '../app.css';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button class="btn">Hello daisyui</button>
+<div class="p-5 grid grid-cols-2 md:grid-cols-3 gap-4">
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F0cbc7fcb-d84a-4ef4-9fbe-0e40081d6958.JPG?alt=media&token=8eb5390e-53d1-4509-96db-6f49ed664462&_gl=1*1ijjl3f*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMxNDEuNjAuMC4w" alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F202da6c0-b437-44e7-9651-58df9913dacf.JPG?alt=media&token=cb234b19-cbec-4094-a06c-97c181ee153e&_gl=1*1fdg7up*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMxOTAuMTEuMC4w" alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F22538c0d-bbe4-4ba6-8122-1caa4eb1e6f4.JPG?alt=media&token=3339cdbe-f070-4794-9bbf-1600ce6fb83f&_gl=1*1lzqt0k*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMyMDMuNjAuMC4w" alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F5aea2216-4a36-49a4-aae1-70bfd2a5c537.JPG?alt=media&token=f184461a-5e29-432d-b3b4-4543be6143fe&_gl=1*1s4t146*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMyNTguNS4wLjA." alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F60e01f7b-b6db-4322-a4f0-cba7bafc3924.JPG?alt=media&token=ab9d3cd0-4df4-4339-a90d-e0c4d44e098d&_gl=1*16o4v8i*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMyNzcuNjAuMC4w" alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F710dad93-fb82-4bcf-98a8-8ec515e856dd.jpeg?alt=media&token=b0725d6c-1e80-4d2c-8fe9-79b60c188940&_gl=1*122sqfd*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMyOTIuNDUuMC4w" alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F71f10a1b-7f8a-40fb-9beb-993d5fd90212.jpeg?alt=media&token=7e315196-a0a3-49d6-8dc7-9d74caa7419e&_gl=1*zlahx8*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMzMTUuMjIuMC4w" alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F7292b006-9fcb-4b2a-b806-26293b957e8b.JPG?alt=media&token=901a481c-d06f-4fb5-97be-878bb35ca526&_gl=1*18nom6x*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMzMzAuNy4wLjA." alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F2fa0d216-415b-4606-aa00-442dc6c57f79.jpeg?alt=media&token=5e120902-12f0-4734-8e8e-fbd4c00dbe4a&_gl=1*944x27*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMzNjMuNDAuMC4w" alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F76c3ca30-db81-4a42-8e50-d3801d6d9674.jpeg?alt=media&token=3bff62c8-0e53-4431-8609-99a546de1372&_gl=1*1qyfnfn*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMzODYuMTcuMC4w" alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F77b16436-e487-4151-a652-49f6f6866b0e.jpeg?alt=media&token=0896f5fc-1b53-4ee3-8803-1548ac95cba3&_gl=1*j5id3e*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzMzOTcuNi4wLjA." alt="">
+	</div>
+	<div>
+	    <img class="h-auto max-w-full rounded-lg" src="https://firebasestorage.googleapis.com/v0/b/photo-log-94d7a.appspot.com/o/beer%2F96042acc-eed2-4e0d-92c0-c1367a9d3cbf.jpeg?alt=media&token=2d86a67d-8f72-4c1a-bcbf-5559b33c53a4&_gl=1*qs78fi*_ga*ODk4MTQyOTA3LjE2OTAyNTQzNDk.*_ga_CW55HF8NVT*MTY5NjczMjkxNi4xOS4xLjE2OTY3MzM0MzMuNTIuMC4w" alt="">
+	</div>
+    </div>
+    
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,37 @@
 module.exports = {
 	content: ['./src/**/*.{svelte,js,ts}'],
-	plugins: [require('daisyui')]
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			'acid',
+			'aqua',
+			'autumn',
+			'black',
+			'bumblebee',
+			'business',
+			'cmyk',
+			'coffee',
+			'corporate',
+			'cupcake',
+			'cyberpunk',
+			'dark',
+			'dracula',
+			'emerald',
+			'fantasy',
+			'forest',
+			'garden',
+			'halloween',
+			'lemonade',
+			'light',
+			'lofi',
+			'luxury',
+			'night',
+			'pastel',
+			'retro',
+			'synthwave',
+			'valentine',
+			'winter',
+			'wireframe'
+		]
+	}
 };
